a24df70ddde1193e56336f8c2080f61af4351a4d
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 12:43:39 +0100

    footer mvp

--- src/components/Library/RecipeCard.jsx
+++ src/components/Library/RecipeCard.jsx
@@ -1,7 +1,37 @@
-const RecipeCard = () => {
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useMemo } from 'react'
+
+const RecipeCard = ({ recipe }) => {
+  const {
+    isSquare,
+    title,
+    price,
+    img
+  } = recipe
+
+  const roundedStyling = 'rounded-full h-[110px] w-[110px] mb-4 z-40'
+  const standardStyling = 'h-[130px] mb-4 z-40'
+
   return (
-    <div>
-      RecipeCard
+    <div className='flex flex-row'>
+      <img src={img} alt={title} className={isSquare ? roundedStyling : standardStyling} />
+      <div className='w-[180px] h-[90px] shadow-lg shadow-grey pl-[25px] pr-[3px] pt-[15px] pb-[3px]'>
+        <div className='flex flex-col gap-7 overflow-hidden'>
+          <div className='font-light text-[12px]'>
+            {title}
+          </div>
+          <div className='flex flex-row justify-between items-center w-full'>
+            <p className='text-dark-green text-[14px]'>
+              {price}
+            </p>
+            <div className='relative'>
+              <div className='absolute text-[12px] flex items-center rounded-full bg-orange h-20 w-20 -top-10 -right-8 text-base'>
+                <AiOutlineShoppingCart />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
