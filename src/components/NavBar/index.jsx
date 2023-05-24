0842148f1927e9eb9b94d14e6ff07a9ba71d6cac
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 10:33:07 +0100

    remove border

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ import NavCTA from './NavCTA'
 const NavBar = () => {
   return (
     <div className='flex flex-row items-center h-[90px] w-full'>
-      <div className='flex justify-center w-2/5 border-solid border-2 border-orange'>
+      <div className='flex justify-center w-2/5'>
         <NavLogo />
       </div>
       <div className='w-3/5 flex flex-row justify-start '>
