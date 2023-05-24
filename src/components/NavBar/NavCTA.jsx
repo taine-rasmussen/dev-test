19985473da6a716897acc397d1c92cc5fd8bcf81
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 11:55:20 +0100

    can i commit?

--- src/components/NavBar/NavCTA.jsx
+++ src/components/NavBar/NavCTA.jsx
@@ -1,16 +1,14 @@
-import Button from '../Library/Button'
+import Button from '../Library/Button';
 
 const NavCTA = () => {
   return (
-    <div className='flex'>
-      <h1 className='font-semibold p-3 whitespace-nowrap hover:text-orange hover:cursor-pointer text-xs'>
+    <div className="flex gap-x-6">
+      <h1 className="font-semibold p-3 whitespace-nowrap hover:text-orange hover:cursor-pointer text-xs">
         Log in
       </h1>
-      <Button primary={false}>
-        CREATE ACCOUNT
-      </Button>
-    </div >
-  )
-}
+      <Button primary={false}>CREATE ACCOUNT</Button>
+    </div>
+  );
+};
 
-export default NavCTA
+export default NavCTA;
