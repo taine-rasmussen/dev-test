3d6e847a3fa71be1338c70cc8f3a2ec546c0bd80
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 10:42:47 +0100

    reduce font weight on secondary btns

--- src/components/Library/Button.jsx
+++ src/components/Library/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ children, primary }) => {
   const primaryStyling = 'p-[19px] text-[16px] font-semibold bg-green whitespace-nowrap px-[35px]'
-  const secondaryStyling = 'border-solid border-black border-2 text-xs p-2 whitespace-nowrap'
+  const secondaryStyling = 'border-solid border-black border-2 text-xs p-2 whitespace-nowrap font-light'
 
   return (
     <button className={primary ? primaryStyling : secondaryStyling}>
--- src/components/NavBar/NavCTA.jsx
+++ src/components/NavBar/NavCTA.jsx
@@ -1,8 +1,15 @@
+import Button from '../Library/Button'
+
 const NavCTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <div className='flex'>
+      <h1 className='font-semibold p-3 whitespace-nowrap hover:text-orange hover:cursor-pointer text-sm'>
+        Log in
+      </h1>
+      <Button primary={false}>
+        CREATE ACCOUNT
+      </Button>
+    </div >
   )
 }
 
