aee58709de50ae1491656cffa9c64c22ac70ecaf
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 10:17:06 +0100

    update tailwind theme with custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green': '#A2DE96',
+        'orange': '#FF947B',
+        'grey': '#00000029',
+        'dark-green': '#4EC6A6',
+        'light-green': '#F1F7EA',
+      },
+    },
   },
   plugins: [],
 }
