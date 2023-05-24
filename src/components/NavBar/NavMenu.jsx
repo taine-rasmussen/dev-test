d4f3a68d51382a781c25ec86c11e3df88315b740
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 10:38:04 +0100

    mock data for menus

--- src/components/NavBar/NavMenu.jsx
+++ src/components/NavBar/NavMenu.jsx
@@ -1,4 +1,24 @@
+import DropDownMenu from '@/components/Library/DropDownMenu'
+
 const NavMenu = () => {
+
+  const productMenuItems = [
+    'Fruit',
+    'Vegetables',
+    'Eggs',
+    'Milk',
+    'Meat',
+    'Fresh drinks'
+  ]
+  const recipeMenuItems = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6'
+  ]
+
   return (
     <div>
       menu
