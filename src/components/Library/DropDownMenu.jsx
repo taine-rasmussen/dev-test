3a3bf382a6151db9baf4b7c6271cb138af5797a0
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 10:37:11 +0100

    reusable drop down menu component

--- src/components/Library/DropDownMenu.jsx
+++ src/components/Library/DropDownMenu.jsx
@@ -1,8 +1,51 @@
-const DropDownMenu = () => {
+import { BsChevronDown } from 'react-icons/bs';
+import { useState } from 'react';
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+} from "@material-tailwind/react";
+
+const DropDownMenu = ({ label, options }) => {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
-    <div>
-      DropDownMenu
-    </div>
+    <Menu
+      open={openMenu}
+      offset={28}
+      handler={setOpenMenu}
+      animate={{
+        mount: { y: 0 },
+        unmount: { y: -25 },
+      }}>
+      <MenuHandler>
+        <div
+          onClick={() => { setOpenMenu(!openMenu) }}
+          className={
+            openMenu
+              ? 'text-orange flex items-center justify-center gap-2 hover:cursor-pointer text-sm'
+              : 'flex items-center justify-center gap-2 hover:cursor-pointer hover:text-orange text-sm'
+          }
+        >
+          {label}
+          <BsChevronDown
+            strokeWidth={1.5}
+            className={`transition-transform ${openMenu ? "rotate-180" : ""}`}
+          />
+        </div>
+      </MenuHandler>
+      <MenuList className='bg-white shadow shadow-grey outline-none'>
+        {options.map(item => (
+          <MenuItem
+            key={item}
+            className='flex mr-20 p-1 hover:text-orange'
+          >
+            {item}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
   )
 }
 
