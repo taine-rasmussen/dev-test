5b301121260886923548db0494aae00d9be23d2f
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-24 15:48:39 +0100

    recipe card icon placement

--- src/components/Library/RecipeCard.jsx
+++ src/components/Library/RecipeCard.jsx
@@ -8,28 +8,29 @@ const RecipeCard = ({ recipe }) => {
     img
   } = recipe
 
-  const roundedStyling = 'rounded-full h-[110px] w-[110px] z-40'
+  const roundedStyling = 'rounded-full h-[110px] w-[110px] z-40 mt-2'
   const standardStyling = 'h-[130px] z-40 mt-2'
-
-  const roundedPositioning = 'w-[180px] h-[90px] shadow-lg shadow-grey pl-[25px] pr-[3px] pt-[2px] pb-[3px] mt-5 -ml-4'
-  const standardPositioning = 'w-[180px] h-[90px] shadow-lg shadow-grey pl-[25px] pr-[3px] pt-[px] pb-[3px] mt-9 -ml-12 mr-5'
+  const roundedPositioning = 'w-[180px] h-[100px] shadow-lg shadow-grey pl-[25px] pr-[5px] pt-[12px]  mt-5 -ml-4'
+  const standardPositioning = 'w-[180px] h-[100px] shadow-lg shadow-grey pl-[25px] pr-[5px] pt-[12px]  mt-8 -ml-12 mr-5'
 
   return (
     <div className='flex flex-row'>
       <img src={img} alt={title} className={isSquare ? roundedStyling : standardStyling} />
       <div className={isSquare ? roundedPositioning : standardPositioning}>
-        <div className='flex flex-col gap-7 overflow-hidden'>
-          <div className='font-light text-[12px]'>
+        <div className='flex flex-col gap-3 overflow-hidden pb-[2px]'>
+          <div className='font-light text-[12px] mr-[16px]'>
             {title}
           </div>
-          <div className='flex flex-row justify-between items-center w-full'>
+          <div className='flex flex-row justify-between items-center w-full px-[5px]'>
             <p className='text-dark-green text-[14px]'>
               {price}
             </p>
-            <div className='relative'>
-              <div className='absolute text-[12px] flex items-center rounded-full bg-orange h-[50px] w-[50px] -top-5 -right-5 text-base'>
-                <AiOutlineShoppingCart />
-              </div>
+            <div className='z-40 -mb-[20px]'>
+              <AiOutlineShoppingCart />
+            </div>
+          </div>
+          <div className='relative'>
+            <div className='absolute text-[12px] flex items-center rounded-full bg-orange h-[65px] w-[65px] -top-9 -right-7 text-base'>
             </div>
           </div>
         </div>
